6071326384fe4ca51ca31525fda5e5bfb90fddf9
Montu Meena <158208396+montumeena141@users.noreply.github.com>
2025-03-31 20:14:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
-    //Write your code here
+    event.preventDefault(); 
 
+  
+    let firstName = document.forms["form1"]["fname"].value.trim();
+    let lastName = document.forms["form1"]["lname"].value.trim();
+
+   
+    if (firstName === "" || lastName === "") {
+        alert("Please enter both First Name and Last Name.");
+        return;
+    }
+
+    // Display full name using alert
+    alert(firstName + " " + lastName);
 }
+
